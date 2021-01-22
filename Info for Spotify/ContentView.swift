46bfdfb74cadbd3038d64b5f61e07fe773075383
louissoccard <louis@soccard.uk>
2021-01-22 18:08:57 +0000

    Initial Commit

---
+++ Info for Spotify/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Info for Spotify
+//
+//  Created by Louis Soccard on 22/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Info for Spotify/Info_for_SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Info_for_SpotifyApp.swift
+//  Info for Spotify
+//
+//  Created by Louis Soccard on 22/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Info_for_SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
